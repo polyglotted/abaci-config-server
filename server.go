c3d4a7f1727cc6c308bcd80cdcc554ddb69b3ef3
Shankar Vasudevan <shankar@polyglotted.io>
2016-11-04 12:00:39 +0000

    intro star

--- server.go
+++ server.go
@@ -12,10 +12,10 @@ import (
 
 func main() {
     rawUrl := flag.String("url", "", "download zip file")
-    var localVol = flag.Bool("local", false, "is local volume")
+    localVol := flag.Bool("local", false, "is local volume")
     flag.Parse()
 
-    if !localVol {
+    if !*localVol {
         log.Println("Downloading ", *rawUrl)
         Download(*rawUrl)
 
